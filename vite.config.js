e4dd113f31131849b7db7de93c1ca4e3a564f27b
sataxd <diegomartinez1996x@gmail.com>
2026-09-03 07:17:43 -0500

    build final

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,6 @@ export default defineConfig({
     // },
     resolve: {
         alias: {
-            'jsencrypt': path.resolve(__dirname, 'node_modules/jsencrypt/bin/jsencrypt.min.js'),
             '@Adminto': path.resolve(__dirname, 'resources/js/Components/Adminto'),
             '@Tailwind': path.resolve(__dirname, 'resources/js/Components/Tailwind'),
             '@Utils': path.resolve(__dirname, 'resources/js/Utils'),
